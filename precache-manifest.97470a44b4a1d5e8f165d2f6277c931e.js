373d3514fe05fc8c4d64b5e627ea71ef997cfe82
ashish kumar rena <ashishkumarrena@ashishs-MacBook-Air.local>
2020-05-01 20:40:53 +0530

    Updates

---
+++ precache-manifest.97470a44b4a1d5e8f165d2f6277c931e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85cfec325cc77107d3d16074ec1aa11",
+    "url": "/Shopping-Site-React-Redux-Hooks/index.html"
+  },
+  {
+    "revision": "74b658b5e11b6b3f346d",
+    "url": "/Shopping-Site-React-Redux-Hooks/static/css/main.a34398e8.chunk.css"
+  },
+  {
+    "revision": "da28eaf9edecc31bbf26",
+    "url": "/Shopping-Site-React-Redux-Hooks/static/js/2.dd04e377.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/Shopping-Site-React-Redux-Hooks/static/js/2.dd04e377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b658b5e11b6b3f346d",
+    "url": "/Shopping-Site-React-Redux-Hooks/static/js/main.dc701ed4.chunk.js"
+  },
+  {
+    "revision": "3b3496b57f8f320266cd",
+    "url": "/Shopping-Site-React-Redux-Hooks/static/js/runtime-main.6d4f5d36.js"
+  }
+]);
